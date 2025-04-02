07d75a1227e2d6efc16d954a146d3629dfb587aa
DataGuti <atvidau@gmail.com>
2025-04-02 10:47:13 -0400

    Format response as json and find dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,5 +6,8 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url,json=input_json,headers=headers)
-
-    return response.text
+    json_response = json.loads(response.text)
+    all_emotions = json_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(all_emotions, key=all_emotions.get)
+    all_emotions['dominant_emotion'] = dominant_emotion
+    return all_emotions
