166d6ceaa714f1fa5e432cf52e7fc76fff25fd4c
DataGuti <atvidau@gmail.com>
2025-04-02 13:02:25 -0400

    Added server and Flask. Ran succesfully on Browser.

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emotion_analysis():
+    text_for_analysis = request.args.get('textToAnalyze')
+    emotion_results = emotion_detector(text_for_analysis)
+    return f"For the given statement, the system response is 'anger': {emotion_results['anger']}, 'disgust': {emotion_results['disgust']}, 'fear': {emotion_results['fear']}, 'joy': {emotion_results['joy']}, 'sadness': {emotion_results['sadness']}. The dominant emotion is {emotion_results['dominant_emotion']}."
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
