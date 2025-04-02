1233c1d1fd15bbffe197b6eb1157294add3090ae
DataGuti <atvidau@gmail.com>
2025-04-02 13:35:49 -0400

    Static Code Analysis completed for server.py

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+"""
+Emotion Detection Server File
+
+This Python script handles the Flask server and connects to the EmotionDetection object.
+
+"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,15 +11,28 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_analysis():
+    """
+    This is the main function to run the emotion analysis.
+    It connects to the EmotionDetection.emotion_detector function.
+    Takes a string as input, returns a JSON object as result.
+    This JSON object has a score for each emotion and a dominant_emotion for the text.
+    """
     text_for_analysis = request.args.get('textToAnalyze')
     emotion_results = emotion_detector(text_for_analysis)
     if emotion_results['dominant_emotion'] is None:
         return "Invalid text! Please try again!."
-    else:
-        return f"For the given statement, the system response is 'anger': {emotion_results['anger']}, 'disgust': {emotion_results['disgust']}, 'fear': {emotion_results['fear']}, 'joy': {emotion_results['joy']}, 'sadness': {emotion_results['sadness']}. The dominant emotion is {emotion_results['dominant_emotion']}."
+    return (
+        f"For the given statement, the system response is 'anger': {emotion_results['anger']}, "
+        f"'disgust': {emotion_results['disgust']}, 'fear': {emotion_results['fear']}, "
+        f"'joy': {emotion_results['joy']}, 'sadness': {emotion_results['sadness']}. "
+        f"The dominant emotion is {emotion_results['dominant_emotion']}."
+    )
 
 @app.route("/")
 def render_index_page():
+    """
+    This function renders the main application page.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
